a8838b0787558a817a5430ae9b3729a871baa335
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2024-01-08 13:47:24 +0300

    Add installation step for yay (#1)
    * chore: Add installation step for yay
    
    * fix: Use correct indentation
    
    * Restyle Add installation step for yay (#2)
    
    * Restyled by autopep8
    
    * Restyled by isort
    
    * Restyled by pyment
    
    * Restyled by shellharden
    
    * Restyled by shfmt
    
    * Restyled by whitespace
    
    ---------
    
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

--- in.sh
+++ in.sh
@@ -12,14 +12,14 @@
 # Check if the user wants interactive or non-interactive mode
 
 if [[ "$1" == "-i" ]]; then
-	INTERACTIVE=true
+  INTERACTIVE=true
 else
-	INTERACTIVE=false
+  INTERACTIVE=false
 fi
 
 # The regex to test if the user accepted (yes) or declined (no)
 yes_regex="^([yY][eE][sS]|[yY])$" # Yes
-no_regex="^([nN][oO]|[nN])$"       # No
+no_regex="^([nN][oO]|[nN])$"      # No
 
 # Install the "global" packages in the list file
 
@@ -32,14 +32,18 @@ FILE_NAME="list"
 # The test can also be "faked" by fakeroot, so let the user run the script
 # If the user is not root, append 'sudo' to the commands that require it.
 
-is_root(){ [ "${EUID:-$(id -u)}" -eq 0 ] }
+is_root() {
+  [ "${EUID:-$(id -u)}" -eq 0 ]
+}
 
 if is_root; then
-	PACMAN_INSTALL="pacman -S"
-	HOMEBREW_INSTALL="brew install"
+  PACMAN_INSTALL="pacman -S --noconfirm"
+  HOMEBREW_INSTALL="brew install"
+  YAY_INSTALL="yay -S --noconfirm"
 else
-	PACMAN_INSTALL="sudo pacman -S"
-	HOMEBREW_INSTALL="sudo brew install"
+  PACMAN_INSTALL="sudo pacman -S --noconfirm"
+  HOMEBREW_INSTALL="sudo brew install"
+  YAY_INSTALL="sudo yay -S --noconfirm"
 fi
 
 # Install the package managers (and update them)
@@ -47,40 +51,75 @@ fi
 # Update pacman (and syncronize packages)
 
 if is_root; then
-	pacman -Syu pacman
+  pacman -Syu pacman
 else
-	sudo pacman -Syu pacman
+  sudo pacman -Syu pacman
 fi
 
 # Install Homebrew
 # Check if the user wants to install homebrew
-if $INTERACTIVE; then
-	read -rp "Install Homebrew?(yes/no)" ihome
-	if [[ $ihome =~ $yes_regex ]]; then
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	fi
+HOMEIBIN=$(/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
+
+if "$INTERACTIVE"; then
+  read -rp "Install Homebrew?(yes/no)" ihome
+  if [[ $ihome =~ $yes_regex ]]; then
+    "$HOMEIBIN"
+  fi
+else
+  # If not interactive, install it
+  "$HOMEIBIN"
+fi
+
+function install_from_aur() {
+  BIN="$1"
+  git clone "https://aur.archlinux.org/$BIN" "/tmp/$BIN" || exit
+  cd "/tmp/$BIN"
+  if "$INTERACTIVE"; then
+    makepkg -simrC
+  else
+    makepkg -simrC --noconfirm
+  fi
+  cd - || exit
+}
+
+# Install Yay
+# Check if yay is there
+ISYAY=false
+
+if command -v "yay"; then
+  ISYAY=true
+fi
+
+# If not installed, ask the user to do so
+if "$INTERACTIVE"; then
+  read -rp "Install yay?(yes/no)" iyay
+  if [[ $iyay =~ $yes_regex ]]; then
+    install_from_aur "yay-bin"
+  fi
+else
+  install_from_aur "yay-bin"
 fi
 
 # Check the file that contains the list of programs to be installed
 # Check what they are using and install them appropritely
 
 while IFS=, read -r package pkm; do
-	# Map the pkm to the appropriate install command
-	if [[ $pkm == "homebrew" ]]; then
-		INSTALL_PKM=$HOMEBREW_INSTALL
-	else 
-		if [[ $pkm == "pacman" ]]; then
-			INSTALL_PKM=$PACMAN_INSTALL
-		fi
-	# Incase there is no package manager specified,
-	# 	use pacman
-	else
-		INSTALL_PKM=$PACMAN_INSTALL
-	fi
-	echo "Installing $package..."
-	echo "$INSTALL_PKM $package"
-	$INSTALL_PKM $package
-done < $FILE_NAME
+  # Map the pkm to the appropriate install command
+  if [[ $pkm == "homebrew" ]]; then
+    INSTALL_PKM=$HOMEBREW_INSTALL
+  elif [[ $pkm == "pacman" ]]; then
+    INSTALL_PKM=$PACMAN_INSTALL
+  elif [[ $pkm == "yay" ]]; then
+    INSTALL_PKM=$YAY_INSTALL
+  # Incase there is no package manager specified,
+  # 	use pacman
+  else
+    INSTALL_PKM=$PACMAN_INSTALL
+  fi
+  echo "Installing $package..."
+  echo "$INSTALL_PKM $package"
+  "$INSTALL_PKM" "$package"
+done <"$FILE_NAME"
 
 # Check the folders in in the current directory
 
@@ -88,7 +127,6 @@ DIRECTORIES_P=$(ls -d */)
 
 IFS="/ "
 
-for DIR in $DIRECTORIES_P; do
-	# Run the scripts of each folder
+for DIR in "$DIRECTORIES_P"; do
+  # Run the scripts of each folder
 done
-
---
+++ waybar/scripts/time.py
@@ -0,0 +1,23 @@
+import json
+import time
+from datetime import datetime
+
+
+def print_current_time():
+    """ """
+    try:
+        while True:
+            current_time = datetime.now().strftime("%H:%M:%S:%f")[:-3]
+            waybar_json = [{"text": current_time, "class": "custom-widget"}]
+            print(json.dumps(waybar_json), flush=True)
+
+            # Sleep for 1 millisecond
+            time.sleep(0.001)
+
+    except KeyboardInterrupt:
+        # Print an empty JSON array when interrupted (e.g., Ctrl+C)
+        print("[]", flush=True)
+
+
+# Call the function to continuously print the current time in Waybar-compatible JSON format
+print_current_time()
